f85e70e40a89e9889eab455857882dab40db4a8e
antonija <antonijapek@gmail.com>
2020-10-28 00:10:23 +0200

    animation updates

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,16 +6,16 @@
                 <h1 class="xl:text-xl lg:text-xl sm:text-lg mobile:text-md font-black leading-none mb-12 mobile:mb-4">{{ header }}</h1>
                 <h2 class="md:text-md sm:text-xmd leading-normal">{{ copy_01 }}</h2>
             </section>
-            <section class="relative" data-aos="fade-up" data-aos-offset="400" data-aos-easing="ease-out">
+            <section class="relative" data-aos="fade-up" data-aos-offset="200" data-aos-easing="ease-out" data-aos-anchor-placement="center-bottom">
                 <h3 class="lg:text-lg leading-none mb-12 mobile:mb-4 sm:text-lg mobile:text-md">Our workflow</h3>
                 <p class="md:text-md sm:text-xmd leading-normal">{{ copy_02 }}</p>
             </section>
-            <section id="contact" class="mt-64 mobile:mt-32 relative" data-aos="fade-up" data-aos-easing="ease-out" data-aos-delay="300" data-aos-anchor-placement="center-bottom">
+            <section class="contact mt-64 mobile:mt-32 relative" data-aos="fade-up" data-aos-easing="ease-out" data-aos-offset="800" data-aos-anchor-placement="center-bottom">
                 <h3 class="lg:text-lg lg:leading-none mb-12 mobile:mb-4 sm:text-lg mobile:text-md">Get in touch</h3>
                 <p class="md:text-md sm:text-xmd leading-normal mb-12">{{ copy_03 }}</p>
             </section>
-            <section data-aos="flip-up" data-aos-easing="ease-out" data-aos-delay="1000" data-aos-anchor-placement="top-bottom">
-                <span class="lg:text-lg sm:text-lg">👉 </span><a href="mailto:hi@chilinotchilly.com" class="lg:text-lg sm:text-lg underline">hi@chilinotchilly.com</a>
+            <section data-aos="flip-up" data-aos-easing="ease-out" data-aos-delay="1000" data-aos-duration="500" data-aos-anchor=".contact" data-aos-offset="200">
+                <span class="lg:text-lg sm:text-lg mobile:text-md">👉 </span><a href="mailto:hi@chilinotchilly.com" class="lg:text-lg sm:text-lg mobile:text-md underline">hi@chilinotchilly.com</a>
             </section>
             <img src="../assets/img/chili-team.png" class="mt-40 mobile:mt-12" alt="Chili team" data-aos="fade-up" data-aos-easing="ease-out" data-aos-duration="1000" data-aos-anchor-placement="bottom-center">
         </div>
