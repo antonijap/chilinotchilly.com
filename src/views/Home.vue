c4b2b94361d7091b90764dc297b1151bd8f88135
antonija <antonijapek@gmail.com>
2020-11-17 00:24:46 +0200

    better chili

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="text-primary overflow-hidden bg-mustard" v-if="copy_01">
         <div class="container mx-auto md:px-12 mobile:px-6">
-        <img src="../assets/img/chili.svg" alt="Chilinotchilly logo" class="absolute top-0 w-2/4 rotate">
+        <img src="../assets/img/chili.svg" alt="Chilinotchilly logo" class="absolute left-0 top-0 w-2/4 rotate">
             <section class="flex content-center flex-wrap xl:mt-40 relative">
                 <img src="../assets/img/logo.svg" alt="Chilinotchilly logo" class="md:mt-24 mobile:mt-24 md:w-32 lg:mb-48">
                 <h1 class="mobile:mt-32 xl:text-xl lg:text-xl sm:text-lg mobile:text-md font-black leading-none mb-12 mobile:mb-4">{{ header }}</h1>
